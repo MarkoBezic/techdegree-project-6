33b0cf1e07fce30e34895b6a5d87ff23f161b96d
Marko Bezic <marko.bezic@gmail.com>
2019-12-03 18:01:57 -0500

    js file not linked???

---
+++ game_show_app_v1.2/js/app.js
@@ -0,0 +1,44 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+var missed = 0;
+const startButton = document.getElementsByClassName('btn__reset');
+const overlay = document.getElementById('overlay');
+const phrases = [
+    "Grape juice is the best",
+    "Birds make great pets",
+    "Comedy makes me laugh",
+    "You win some you lose some",
+    "Exercise is a must",
+];
+
+
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// add the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+   overlay.style.display = 'none';
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
