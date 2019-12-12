2b7c2e0eca7a168af04d2be37b966bce564aa7bf
Marko Bezic <marko.bezic@gmail.com>
2019-12-12 18:15:07 -0500

    ul not a node?

--- game_show_app_v1.2/js/app.js
+++ game_show_app_v1.2/js/app.js
@@ -4,6 +4,7 @@ const phrase = document.getElementById('phrase');
 const qwerty = document.getElementById('qwerty');
 var missed = 0
 
+
 const phrases = [
     "Grape juice is the best",
     "Birds make great pets",
@@ -24,9 +25,12 @@ function getRandomPhraseAsArray(arr){
 }
 
 
+
 //Adding phrase letters to display
+const randomPhraseLetters = getRandomPhraseAsArray(phrases);
+
 function addPhraseToDisplay(arr){
-    const randomPhraseLetters = getRandomPhraseAsArray(arr);
+   
     for (let i = 0; i < randomPhraseLetters.length; i ++){
         const ul = document.querySelector('ul');
         const li = document.createElement('li');
@@ -36,7 +40,50 @@ function addPhraseToDisplay(arr){
         }else{
             li.className = 'space';
         }
-        ul.appendChild('li');
+        ul.appendChild(li);
     }
 }
-addPhraseToDisplay(phrases);
+addPhraseToDisplay(randomPhraseLetters);
+
+// Create checkLetter function
+    //checkLetter Function with parameeter for button that gets clicked
+function checkLetter(button) {
+    //store all of the li elements in a variable 'letters'
+    const letters = getElementsByClassName('letter');
+    //creat a variable to store if a match is found and give it an initial value of null
+    let match = null;
+    //loop through all of the li elements
+    for (let i = 0; i < letters.length; i ++){
+        //create a conditional that compares the text of the button parameter to the text of the li at the current index of the loop
+        if ( li[i].textContent = button) {
+            //if they match, add the 'show' class to the li
+            li.className = 'show';
+            //if they match, store the button text in the match variable
+            match = button.textContent;
+        } 
+    }
+    // return the match variable
+    return match;
+    console.log(match);
+}
+
+
+//add event listener to the keyboard
+    //create an event listener for the qwerty element that listens for the 'clicl' event
+qwerty.addEventListener('click', (e) => {
+    button = e.target
+    //create a conditional the filter out clicks not on buttons and any chosen buttons
+    if ( button === 'BUTTON' && button !== 'chosen'){
+        //add the 'chosen' class to the button that was pressed.
+        button.className = 'chosen';
+        //call the checkLetter function and store the results in a variable
+        let checkedLetter = checkLetter(button);
+        //if the checkLetter function does not find a letter, remove one of the heart images and increment the missed counter
+        if (checkedLetter = null){
+            const ol = document.querySelector('ol');
+            ol.removeChild('li');
+            missed += 1;
+        }
+    }
+});
+
